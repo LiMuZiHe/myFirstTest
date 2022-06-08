3fd076da59518b9d2b95d6c49d0a7a161323fed7
LiMuZiHe <107088912+LiMuZiHe@users.noreply.github.com>
2022-06-08 10:41:26 +0800

    Add files via upload

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
-module wxcloudrun-golang
+module wxproject1
 
 go 1.16
 
 require (
-	gorm.io/driver/mysql v1.1.2
-	gorm.io/gorm v1.21.16
+	github.com/gin-gonic/gin v1.8.0
+	gopkg.in/ini.v1 v1.66.6
+	gorm.io/driver/mysql v1.3.4
+	gorm.io/gorm v1.23.5
 )
--- main.go
+++ main.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"wxcloudrun-golang/db"
-	"wxcloudrun-golang/service"
+	"os"
+	"wxproject1/dao"
+	"wxproject1/routers"
+	"wxproject1/setting"
 )
 
 func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
+	if len(os.Args) < 2 {
+		fmt.Println("Usage：./wx conf/config.ini")
+		return
+	}
+	// 加载配置文件
+	if err := setting.Init(os.Args[1]); err != nil {
+		fmt.Printf("load config from file failed, err:%v\n", err)
+		return
+	}
+	// 创建数据库
+	// sql: CREATE DATABASE bubble;
+	// 连接数据库
+	err := dao.InitMySQL(setting.Conf.MySQLConfig)
+	if err != nil {
+		fmt.Printf("init mysql failed, err:%v\n", err)
+		return
 	}
 
-	http.HandleFunc("/", service.IndexHandler)
-	http.HandleFunc("/api/count", service.CounterHandler)
-
-	log.Fatal(http.ListenAndServe(":80", nil))
+	// 注册路由
+	r := routers.SetupRouter()
+	if err := r.Run(fmt.Sprintf(":%d", setting.Conf.Port)); err != nil {
+		fmt.Printf("server startup failed, err:%v\n", err)
+	}
 }
